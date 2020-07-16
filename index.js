9282f2b60b60c6773400794553f11b2ead5c5d7e
mathieucolla@gmail.com <mathieucolla@gmail.com>
2020-07-16 16:40:36 +0200

    init testjs for server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express() ;
+const server = app.listen(4000);
+
+console.log('running nodeJsTest on port 4000');
+
+app.get('/', (req, res) =>{
+    console.log('getting base root');
+    res.status(200).send('test ok');
+});
