b2edd432a46cdece0097c68447ff6ad2563e4a1a
Math17 <mathieucolla@gmail.com>
2020-08-14 17:03:14 +0200

    change port to 80

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const app = express() ;
-const server = app.listen(4000);
+const express = require("express");
+const app = express();
+const server = app.listen(80);
 
-console.log('running nodeJsTest on port 4000');
+console.log("running nodeJsTest on port 4000");
 
-app.get('/', (req, res) =>{
-    console.log('getting base root');
-    res.status(200).send('test ok');
+app.get("/", (req, res) => {
+  console.log("getting base root");
+  res.status(200).send("test ok");
 });
