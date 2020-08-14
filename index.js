49deb597c4c27b74d19a9c3c58be3aba0a6d3154
Math17 <mathieucolla@gmail.com>
2020-08-14 19:35:56 +0200

    add https with cert 443

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const http = require("http");
+const https = require("https");
+const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const server = http.createServer(app);
 server.listen(80);
+
+const server2 = https.createServer(
+  {
+    key: fs.readFileSync(path.join(__dirname, "/ssl/key.pem")),
+    cert: fs.readFileSync(path.join(__dirname, "/ssl/cert.pem")),
+  },
+  app
+);
+server2.listen(443);
 
 console.log("running nodeJsTest on port 80");
 
